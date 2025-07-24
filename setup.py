75cc178a2234f3d8ca356f3233ae5a11e1f8365b
Luís Garrote <80787932+luisgarrote@users.noreply.github.com>
2025-07-24 16:30:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 	long_description = f.read()
 
-setup(name='gym_dino',
-	version='0.0.4',
+setup(name='gym_dino2',
+	version='0.0.1',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
 	install_requires=['gym', 'pygame'],
